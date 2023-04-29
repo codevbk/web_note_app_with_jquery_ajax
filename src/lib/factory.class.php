03340015da07327725eb7e9d9d38b179e74b3922
codevbk <123780171+codevbk@users.noreply.github.com>
2023-04-29 20:00:54 +0300

    Updated factory class
    - Added abstract factory interface & concrete factory method
    - Updated factory constructor & functions

--- src/lib/factory.class.php
+++ src/lib/factory.class.php
@@ -1,21 +1,44 @@
 <?php
-// Factory Method
-class NoteFactory {
+// Abstract Factory Interface
+interface NoteFactoryInterface {
+    public function createNoteModel();
+    public function getNoteModel();
+    public function createNoteView();
+    public function getNoteView();
+    public function createNoteController();
+    public function getNoteController();
+}
+
+
+// Concrete Factory Method
+class NoteFactory implements NoteFactoryInterface{
 	public $model;
 	public $view;
 	public $controller;
     public function __construct() {
-        $this->model = new NoteModel();
-        $this->view = new NoteView($this->model);
-        $this->controller = new NoteController($this->model, $this->view);
+        $this->model = $this->createNoteModel();
+        $this->view = $this->createNoteView();
+        $this->controller = $this->createNoteController();
+	}
+	
+	public function createNoteModel() {
+		return new NoteModel();
 	}
 	
 	public function getNoteModel() {
 		return $this->model;
 	}
 	
+	public function createNoteView() {
+		return new NoteView($this->model);
+	}
+	
 	public function getNoteView() {
 		return $this->view;
+	}
+	
+	public function createNoteController() {
+		return new NoteController($this->model, $this->view);
 	}
 	
 	public function getNoteController() {
